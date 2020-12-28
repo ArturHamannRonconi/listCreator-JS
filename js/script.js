5252d2d5905cf3df9d7f41959ab5ac59e7d019f9
Artur Hamann Ronconi <arturhronconi@gmail.com>
2020-12-27 22:04:58 -0300

    terminei a parte de criar listas

--- js/script.js
+++ js/script.js
@@ -3,17 +3,27 @@ const links = document.querySelectorAll(".link");
 const sectionLists = document.querySelector("#section--1");
 const sectionCreate = document.querySelector("#section--2");
 const buttonCreate = document.querySelector("#section--2 .button");
+const inputs = document.querySelectorAll("input");
 
 
 
 // variáveis
 let listsJson = [];
+let send;
 let listCreator = {
     creat:() => {
-        const inputs = document.querySelectorAll("input");
-        const checkInput = listCreator.checkInputs(inputs);
-        
-        if(checkInput === true){
+        listCreator.clearErrors(inputs);
+        for(let input of inputs){
+            let checkInput = listCreator.checkInputs(input);
+            if(checkInput !== true){
+                send = false;
+                listCreator.showError(input, checkInput);
+            }
+            else{
+                send = true;
+            }
+        }
+        if(send){
             const title = inputs[0].value;
             const listItens = inputs[1].value.split(",");
 
@@ -23,11 +33,6 @@ let listCreator = {
 
             console.log(listsJson);
         }
-        else{
-            // let errorMensage = "Os dois campos precisam ser preenchidos com um título e pelo menos 1 item";
-            // listCreator.showError(input, errorMensage);
-            console.log(checkInput)
-        }
 
     },
     clearIputs:(inputs) => {
@@ -35,24 +40,33 @@ let listCreator = {
             input.value = ""
         };
     },
-    checkInputs:(inputs) => {
-        for(let input of inputs){
-            switch (input.value) {
-                case "":
-                    return "Esse campo precisa ser preenchido";
-                    break;
-            }
+    checkInputs:(input) => {
+        switch (input.value) {
+            case "":
+                return "Esse campo precisa ser preenchido";
+                break;
         }
         return true;
     },
     showError:(input, error) => {
         const errorElement = document.createElement("div");
-        const label = document.querySelector(".list-create-area");
         errorElement.classList.add("error");
         errorElement.innerHTML = error;
+        input.style.borderColor = "tomato";
+        input.style.borderTopLeftRadius = "0";
+        input.style.borderBottomLeftRadius = "0";
 
-        label.insertBefore(errorElement, input.nextElementSibling)
-
+        input.parentElement.insertBefore(errorElement, input)
+    },
+    clearErrors:(inputs) => {
+        let errorElements = document.querySelectorAll(".error");
+        
+        for(let input of inputs){
+            input.style = "";
+        }
+        for(let error of errorElements){
+            error.remove();
+        }
     }
 };
 
@@ -65,16 +79,21 @@ for(let link of links){
     link.addEventListener("click", (e) => {
         e.preventDefault();
 
+        let linkMenu;
+
         switch(link.getAttribute("data-link")){
             case "lists":
+                linkMenu = links[0]
                 clearScreen();   
                 sectionLists.classList.remove("none");
-                link.classList.add("active");
+                linkMenu.classList.add("active");
                 break;
             case "create":
+                linkMenu = links[1]
                 clearScreen();
+                listCreator.clearIputs(inputs)
                 sectionCreate.classList.remove("none");
-                link.classList.add("active");
+                linkMenu.classList.add("active");
                 break;
         };
     });
