9e6bd6b37bdf6d7d1fdec0a56b04679927f27119
Artur Hamann Ronconi <arturhronconi@gmail.com>
2020-12-29 23:25:56 -0300

    começando a arrumar a parte de edição

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
-// constantes
-const links = document.querySelectorAll(".link");
 const sectionLists = document.querySelector("#section--1");
 const sectionCreate = document.querySelector("#section--2");
 const buttonCreate = document.querySelector("#section--2 .button");
 const inputs = document.querySelectorAll("input");
 
-
-
-// variáveis
 let listsJson = [];
 let listCreator = {
     creat:() => {
         let send = true;
-
+        
         listCreator.clearErrors(inputs);
-
+        
         for(let input of inputs){
             let checkInput = listCreator.checkInputs(input);
             if(checkInput !== true){
@@ -33,7 +28,7 @@ let listCreator = {
             
             listsJson.map((list) => {
                 const listClone = document.querySelector(".list--area").cloneNode(true);
-
+                
                 listClone.querySelector("h2").innerHTML = list.title;
                 for(let item of list.listItens){
                     let listItem = `<li>${item}</li>`;
@@ -62,7 +57,7 @@ let listCreator = {
         const errorElement = document.createElement("div");
         errorElement.classList.add("error");
         errorElement.innerHTML = error;
-        input.classList.add("inputError");
+        input.classList.add("input--error");
 
         input.parentElement.insertBefore(errorElement, input.nextElementSibling);
     },
@@ -70,7 +65,7 @@ let listCreator = {
         let errorElements = document.querySelectorAll(".error");
         
         for(let input of inputs){
-            input.classList.remove("inputError");
+            input.classList.remove("input--error");
         }
         for(let error of errorElements){
             error.remove();
@@ -80,9 +75,55 @@ let listCreator = {
 
 
 buttonCreate.addEventListener("click", listCreator.creat);
+for(let input of inputs){
+    input.addEventListener("keypress", (e) => {
+        if(e.key === "Enter"){
+            listCreator.creat();
+        }
+    });
+}
+
+
+
+const settings = document.querySelector(".edit-icon--area");
+const tools = document.querySelector(".edit-list--area");
+const back = document.querySelector(".back")
+
+settings.addEventListener("click", () => {
+    settings.querySelector(".icons").classList.remove("girando");
+    settings.querySelector(".icons").classList.add("girando");
+    settings.style.width = "0%";
+    setTimeout(() => {
+        settings.classList.add("none");
+        tools.classList.remove("none");
+        setTimeout(() => {
+            tools.style.width = "15%";
+        }, 10);
+    }, 500);
+});
+
+back.addEventListener("click", () => {
+    settings.querySelector(".icons").classList.remove("girando");
+    tools.style.width = "0%";
+    setTimeout(() => {
+        settings.classList.remove("none");
+        setTimeout(() => {
+            settings.style = "";
+            setTimeout(() => {
+                settings.querySelector(".icons").classList.add("girando");
+            }, 0);
+        }, 10)
+    }, 450);
+    setTimeout(() => {
+        tools.classList.add("none");
+    }, 400);
+});
+
 
 
 // dando função aos links
+const links = document.querySelectorAll(".link");
+
 for(let link of links){
     link.addEventListener("click", (e) => {
         e.preventDefault();
